dcfeb58bac365622083578c7e68b36c71ce5f6b7
Deák Tamás <deaktamas1994@gmail.com>
2025-07-15 22:03:33 +0200

    update

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Belépési adatok:', this.loginForm.value);
+      // Itt hívd a backend szolgáltatást vagy más logikát
+    } else {
+      this.loginForm.markAllAsTouched(); // jelzi a hibákat, ha submitnál érvénytelen
+    }
+  }
+}
